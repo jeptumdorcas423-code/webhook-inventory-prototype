b485e45f3ad4d6f08b529c3425623e0d7ff06107
jeptumdorcas423-code <jeptumdorcas423@gmail.com>
2026-08-20 12:44:19 +0300

    Move webhook secret to environment variable

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from fastapi import FastAPI, Header, HTTPException
 
 app = FastAPI()
 
 stock = {}
 
-WEBHOOK_SECRET = "my-secret-key"
+WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
 
 @app.get("/")
 def home():
@@ -14,6 +15,7 @@ def home():
 def receive_webhook(data: dict, x_webhook_secret: str = Header(None)):
     if x_webhook_secret != WEBHOOK_SECRET:
         raise HTTPException(status_code=401, detail="Invalid secret")
+
     product = data["product"]
     quantity = data["stock"]
 
@@ -24,6 +26,7 @@ def receive_webhook(data: dict, x_webhook_secret: str = Header(None)):
         "product": product,
         "stock": quantity
     }
+
 @app.get("/stock/{product}")
 def get_stock(product: str):
     quantity = stock.get(product, 0)
